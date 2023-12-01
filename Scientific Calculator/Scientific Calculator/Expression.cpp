82fecf79cc6d82fcbec9408eac383ca49fd32970
Serban Timofte <serbantimofte@Serbans-MacBook-Air.local>
2023-12-01 12:07:37 +0200

    Expression class updated

--- Scientific Calculator/Scientific Calculator/Expression.cpp
+++ Scientific Calculator/Scientific Calculator/Expression.cpp
@@ -24,10 +24,64 @@ bool Expression::evaluate() {
     return isValid;
 }
 
-double Expression::getResult() const {
+double Expression::getResult() {
     return result;
 }
 
-bool Expression::isValidExpression() const {
+bool Expression::isValidExpression() {
     return isValid;
+}
+
+string Expression::get_rawExpression() {
+    return this->rawExpression;
+}
+
+double Expression::get_result() {
+    return this->result;
+}
+
+bool Expression::get_isValid() {
+    return this->isValid;
+}
+
+void Expression::set_rawExpression(const string& expr) {
+    if (expr.length() <= MAX_LEN) {
+        this->rawExpression = expr;
+    }
+    else { throw exception("Invalid expresion tried to be assigned!"); }
+}
+
+void Expression::set_result(double res) {
+    this->result = res;
+}
+
+void Expression::set_isValid(bool val) {
+    this->isValid = val;
+}
+
+ostream& operator<<(ostream& cons, const Expression& expr) {
+    cons << "Expression: " << expr.rawExpression << ", Result: " << expr.result << ", IsValid: " << expr.isValid;
+    return cons;
+}
+
+istream& operator>>(istream& cons, Expression& expr) {
+    cout << "Enter expression: ";
+    getline(cons, expr.rawExpression);
+    expr.isValid = false;
+    return cons;
+}
+
+Expression Expression::operator+(const Expression& e) const {
+    return Expression(this->rawExpression + e.rawExpression);
+}
+
+Expression& Expression::operator++() {
+    ++result;
+    return *this;
+}
+
+Expression Expression::operator++(int) {
+    Expression temp = *this;
+    result++;
+    return temp;
 }
--- Scientific Calculator/Scientific Calculator/Expression.h
+++ Scientific Calculator/Scientific Calculator/Expression.h
@@ -11,11 +11,27 @@ private:
     double result;
     bool isValid;
 
+    const int MAX_LEN = 1000;
 public:
     Expression();
     explicit Expression(const string& expr);
     void setExpression(const string& expr);
     bool evaluate();
-    double getResult() const;
-    bool isValidExpression() const;
+    double getResult();
+    bool isValidExpression();
+
+    string get_rawExpression();
+    double get_result();
+    bool get_isValid();
+
+    void set_rawExpression(const string& expr);
+    void set_result(double res);
+    void set_isValid(bool val);
+
+    friend ostream& operator<<(ostream& os, const Expression& expr);
+    friend istream& operator>>(istream& is, Expression& expr);
+
+    Expression operator+(const Expression& rhs) const;
+    Expression& operator++();  
+    Expression operator++(int);
 };
