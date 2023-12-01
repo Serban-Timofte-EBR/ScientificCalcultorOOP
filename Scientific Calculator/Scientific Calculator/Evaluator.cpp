abf9460bbfec2dbdd923021d94be21b12a4c8040
Serban Timofte <serbantimofte@Serbans-MacBook-Air.local>
2023-12-01 10:39:48 +0200

    Updated Evaluator class

--- Scientific Calculator/Scientific Calculator/Evaluator.cpp
+++ Scientific Calculator/Scientific Calculator/Evaluator.cpp
@@ -1,40 +1,115 @@
 #include "Evaluator.h"
 #include <stdexcept>
+#include <cmath>
 
-Evaluator::Evaluator(Token* tokens, int count) : tokens(tokens), tokenCount(count) {}
+Evaluator::Evaluator(Token* tokens, int count)
+    : tokens(tokens), tokenCount(count), operands(nullptr), operators(nullptr),
+    operandsSize(0), operandsCapacity(10), operatorsSize(0), operatorsCapacity(10) {
+    operands = new double[operandsCapacity];
+    operators = new string[operatorsCapacity];
+}
+
+Evaluator::~Evaluator() {
+    delete[] operands;
+    delete[] operators;
+}
+
+void Evaluator::resizeOperands() {
+    int newCapacity = operandsCapacity * 2;
+    double* newOperands = new double[newCapacity];
+    for (int i = 0; i < operandsSize; ++i) {
+        newOperands[i] = operands[i];
+    }
+    delete[] operands;
+    operands = newOperands;
+    operandsCapacity = newCapacity;
+}
+
+void Evaluator::resizeOperators() {
+    int newCapacity = operatorsCapacity * 2;
+    string* newOperators = new string[newCapacity];
+    for (int i = 0; i < operatorsSize; ++i) {
+        newOperators[i] = operators[i];
+    }
+    delete[] operators;
+    operators = newOperators;
+    operatorsCapacity = newCapacity;
+}
+
+void Evaluator::pushOperand(double value) {
+    if (operandsSize == operandsCapacity) {
+        resizeOperands();
+    }
+    operands[operandsSize++] = value;
+}
+
+void Evaluator::pushOperator(const string& op) {
+    if (operatorsSize == operatorsCapacity) {
+        resizeOperators();
+    }
+    operators[operatorsSize++] = op;
+}
+
+double Evaluator::popOperand() {
+    if (operandsSize == 0) {
+        throw runtime_error("Invalid expression");
+    }
+    return operands[--operandsSize];
+}
+
+string Evaluator::popOperator() {
+    if (operatorsSize == 0) {
+        throw runtime_error("Invalid expression");
+    }
+    return operators[--operatorsSize];
+}
+
+void Evaluator::applyOperator(const string& op) {
+    if (operandsSize < 2) {
+        throw runtime_error("Invalid expression");
+    }
+    double right = popOperand();
+    double left = popOperand();
+
+    if (op == "+") pushOperand(left + right);
+    else if (op == "-") pushOperand(left - right);
+    else if (op == "*") pushOperand(left * right);
+    else if (op == "/") {
+        if (right == 0) throw runtime_error("Division by zero");
+        pushOperand(left / right);
+    }
+    else {
+        throw runtime_error("Unknown operator");
+    }
+}
+
+int Evaluator::getPrecedence(const string& op) {
+    if (op == "+" || op == "-") return 1;
+    if (op == "*" || op == "/") return 2;
+    return 0; 
+}
 
 double Evaluator::evaluate() {
-    stack<double> values;
-
     for (int i = 0; i < tokenCount; ++i) {
-        if (tokens[i].getType() == TokenType::NUMBER) {
-            values.push(stod(tokens[i].getValue()));
+        if (tokens[i].get_type() == TokenType::NUMBER) {
+            pushOperand(stod(tokens[i].get_value()));
         }
-        else if (tokens[i].getType() == TokenType::OPERATOR) {
-            if (values.size() < 2) {
-                throw runtime_error("Invalid expression");
+        else if (tokens[i].get_type() == TokenType::OPERATOR) {
+            string op = tokens[i].get_value();
+            while (operatorsSize > 0 && getPrecedence(operators[operatorsSize - 1]) >= getPrecedence(op)) {
+                applyOperator(popOperator());
             }
-            double right = values.top();
-            values.pop();
-            double left = values.top();
-            values.pop();
-
-            switch (tokens[i].getValue()[0]) {
-            case '+': values.push(left + right); break;
-            case '-': values.push(left - right); break;
-            case '*': values.push(left * right); break;
-            case '/':
-                if (right == 0) throw runtime_error("Division by zero");
-                values.push(left / right);
-                break;
-            default: throw runtime_error("Unknown operator");
-            }
+            pushOperator(op);
         }
     }
 
-    if (values.size() != 1) {
+    while (operatorsSize > 0) {
+        applyOperator(popOperator());
+    }
+
+    if (operandsSize != 1) {
         throw runtime_error("Invalid expression");
     }
 
-    return values.top();
+    return popOperand();
 }
--- Scientific Calculator/Scientific Calculator/Evaluator.h
+++ Scientific Calculator/Scientific Calculator/Evaluator.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "Token.h"
-#include <stack>
 #include <string>
 
 using namespace std;
@@ -9,8 +8,22 @@ class Evaluator {
 private:
     Token* tokens;
     int tokenCount;
+    double* operands; 
+    string* operators; 
+    int operandsSize, operandsCapacity;
+    int operatorsSize, operatorsCapacity;
+
+    void resizeOperands();
+    void resizeOperators();
+    void pushOperand(double value);
+    void pushOperator(const string& op);
+    double popOperand();
+    string popOperator();
+    void applyOperator(const string& op);
+    int getPrecedence(const string& op);
 
 public:
     Evaluator(Token* tokens, int count);
+    ~Evaluator();
     double evaluate();
 };
